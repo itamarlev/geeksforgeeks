f6747e260a976cedb19d6c4bb1e660cdf50f8129
Itamar Lev <itamar.lev@gmail.com>
2018-07-27 20:28:38 +0300

    Create LongestSubstringWithEqualNumberOfZerosAndOnes.java
    Given a binary string, the task is to find maximum substring length with equal number of 0’s and 1’s.
    
    Input:
    The first line of input contains an integer T denoting the number of test cases. Then T test cases follow. Each test case contains a binary string.
    
    Output:
    For each test case, print the maximum substring length in new line.
    
    Constraints:
    1<=T<=100
    1<=|String length|<=105
    
    Example:
    Input:
    2
    1100011
    0010
    
    Output:
    6
    2

---
+++ src/geeksforgeeks/LongestSubstringWithEqualNumberOfZerosAndOnes.java
@@ -0,0 +1,45 @@
+import java.lang.*;
+import java.io.*;
+class GFG
+ {
+	public static void main (String[] args)
+	 {
+	 Scanner s= new Scanner(System.in);
+	 int t=s.nextInt();
+	 
+	 while(t>0)
+	 {
+	     String str=s.next();
+	     HashMap<Integer,Integer> map=new HashMap<>();
+	     
+	     int countOne=0;
+	     int countZero=0;
+	     int max=-1;
+	     for(int i=0;i<str.length();i++)
+	     {
+	         if(str.charAt(i)=='0')
+	         countZero++;
+	         else
+	         countOne++;
+	         
+	         if(map.containsKey(countOne-countZero) || countOne-countZero==0)
+	         {
+	             if(countOne-countZero==0)
+	             {
+	                 max=Math.max(i+1,max);
+	             }
+	             else if(max<(i-map.get(countOne-countZero)))
+	             {
+	                 max=i-map.get(countOne-countZero);
+	             }
+	         }
+	         else
+	         map.put(countOne-countZero,i);
+	     }
+	     
+	     System.out.println(max);
+	    t--;    
+	 }
+	 
+	 }
+}
